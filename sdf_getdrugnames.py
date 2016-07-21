3733394f8316246e05a0c557a1db2869374598ce
Nivi-Sid <vidya852k@gmail.com>
2016-07-21 18:59:19 -0500

    Update sdf_getdrugnames.py

--- sdf_getdrugnames.py
+++ sdf_getdrugnames.py
@@ -1,10 +1,10 @@
-# This python script extracts a specific field from an sdf file.
+# This python script extracts a specific field from an sdf file. This script is an example to extract the drugnames.
 
 import sys, os
 inputf = open("structures.sdf","r")
 
 prevline = ""
-o = open("drugnames","w")
+o = open("drugnames","w") #drugnames is the output file 
 for currline in inputf:
 	
 	if "<GENERIC_NAME>" in prevline:
