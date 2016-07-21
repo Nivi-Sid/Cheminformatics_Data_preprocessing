874aafb66d51b7478f19e3f20c4e45e6be0e074d
Nivi-Sid <vidya852k@gmail.com>
2016-07-21 10:32:57 -0500

    Update sdfsplit.py
    Commented the code

--- sdfsplit.py
+++ sdfsplit.py
@@ -1,13 +1,13 @@
 import sys, os
-max_molecules_per_file = 1
-input_sdf = open("structures.sdf", "r")
+max_molecules_per_file = 1 #write 1 molecule per file
+input_sdf = open("structures.sdf", "r") #structures.sdf is the file name
 counter = 0
 ind =1
 mol = " "
-o = open( "output_" + str(counter) + ".sdf", "w")
+o = open( "output_" + str(counter) + ".sdf", "w") #output file to write the molecule
 for line in input_sdf:
     mol += line
-    if "$$$$" in line:
+    if "$$$$" in line: #delimiter that separated two molecules
 	
         o.write( mol )
 	
